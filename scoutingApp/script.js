9a8f841c3682b0baba266c35ebed004feef8452b
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2019-09-10 12:19:47 -0500

    added web code for saving files

---
+++ scoutingApp/script.js
@@ -0,0 +1,9 @@
+//open navbar
+function openNav(){
+    document.getElementById("mySidenav").style.width= "250px";
+}
+
+//closing navbar
+function closeNav(){
+document.getElementById("mySidenav").style.width="0";
+}
